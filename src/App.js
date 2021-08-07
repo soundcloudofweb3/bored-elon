662f141535784b91e62b6187170871960c083153
reneDescartess <you@example.com>
2021-08-07 16:40:19 -0700

    web3 only

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bored Elon Unicorn club<code>src/App.js</code> .
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/BoredElonUC"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
